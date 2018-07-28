d12308167c30085b9fb7f4c8050c106cb05be1c5
alveis <michaelngiri@gmail.com>
2018-07-28 12:32:47 +0100

    complete wordAdapter java class

--- app/src/main/java/com/alveis/multiscreen_java/WordAdapter.java
+++ app/src/main/java/com/alveis/multiscreen_java/WordAdapter.java
@@ -1,15 +1,36 @@
 package com.alveis.multiscreen_java;
 
+import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word>{
+    public WordAdapter(Activity context, ArrayList<Word> words){
+        super(context, 0, words);
+    }
+    private static final String LOG_TAG = WordAdapter.class.getSimpleName();
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View listItemView = convertView;
+        if (listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+
+        }
+        Word currentWord =getItem(position);
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(currentWord.getMiworkTranslation());
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
         return super.getView(position, convertView, parent);
     }
 }
